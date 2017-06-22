9511242ada666d9915435588c214cf6315fc5e89
Cristian21395 <Cristian.Gomez2@alu.uclm.es>
2017-06-22 10:25:01 +0200

    Identation

--- DrobotsConIceGrid/code/Player.py
+++ DrobotsConIceGrid/code/Player.py
@@ -13,105 +13,105 @@ import drobots
 import sys, time, random
 
 class PlayerApp(Ice.Application): 
-    def run(self, argv):
+	def run(self, argv):
 
-        broker = self.communicator()
+		broker = self.communicator()
 
-        #well-known object
-        container_proxy = broker.stringToProxy('container')
-        container = services.ContainerPrx.checkedCast(container_proxy)
+		#well-known object
+		container_proxy = broker.stringToProxy('container')
+		container = services.ContainerPrx.checkedCast(container_proxy)
 
-        adapter = broker.createObjectAdapter('PlayerAdapter')
-        adapter.activate()
+		adapter = broker.createObjectAdapter('PlayerAdapter')
+		adapter.activate()
 
-        player_servant = PlayerI(broker, adapter, container)
-        proxy_player = adapter.addWithUUID(player_servant)
-        print ('Proxy player: ' +str(proxy_player))
-        direct_player = adapter.createDirectProxy(proxy_player.ice_getIdentity())
-        player = drobots.PlayerPrx.uncheckedCast(direct_player)
+		player_servant = PlayerI(broker, adapter, container)
+		proxy_player = adapter.addWithUUID(player_servant)
+		print ('Proxy player: ' +str(proxy_player))
+		direct_player = adapter.createDirectProxy(proxy_player.ice_getIdentity())
+		player = drobots.PlayerPrx.uncheckedCast(direct_player)
 
-        proxy_game = broker.propertyToProxy('Player') 
-        print ('Proxy game: ' +str(proxy_game))
-        #game = drobots.GamePrx.checkedCast(proxy_game)
-        gameFact = drobots.GameFactoryPrx.checkedCast(proxy_game)
-        game = gameFact.makeGame("GameRobots", 2)
-        
-        try:
-            print ('Trying to do login...')
-            game.login(player, 'Cris' + str(random.randint(0,99)))
-            print ('Waiting to receive both the robot controllers and the detector')
-        except drobots.GameInProgress:
-            print "\nGame in progress. Try it again"
-            return 1
-        except drobots.InvalidProxy:
-            print "\nInvalid proxy"
-            return 2
-        except drobots.InvalidName, e:
-            print "\nInvalid name. It is possible that other person be using your name"
-            print str(e.reason)
-            return 3
-        except drobots.BadNumberOfPlayers:
-            print "\nBad number of players"
-            return 4           
-        
-        sys.stdout.flush()
-        self.shutdownOnInterrupt()
-        broker.waitForShutdown()
-        return 0
+		proxy_game = broker.propertyToProxy('Player') 
+		print ('Proxy game: ' +str(proxy_game))
+		game = drobots.GamePrx.checkedCast(proxy_game)
+		gameFact = drobots.GameFactoryPrx.checkedCast(proxy_game)
+		game = gameFact.makeGame("GameRobots", 2)
+		
+		try:
+			print ('Trying to do login...')
+			game.login(player, 'Cris' + str(random.randint(0,99)))
+			print ('Waiting to receive both the robot controllers and the detector')
+		except drobots.GameInProgress:
+			print "\nGame in progress. Try it again"
+			return 1
+		except drobots.InvalidProxy:
+			print "\nInvalid proxy"
+			return 2
+		except drobots.InvalidName, e:
+			print "\nInvalid name. It is possible that other person be using your name"
+			print str(e.reason)
+			return 3
+		except drobots.BadNumberOfPlayers:
+			print "\nBad number of players"
+			return 4           
+		
+		sys.stdout.flush()
+		self.shutdownOnInterrupt()
+		broker.waitForShutdown()
+		return 0
 
 class PlayerI(drobots.Player):
-    def __init__(self, broker, adapter, container):
-        self.broker = broker
-        self.adapter = adapter
-        self.container = container    
-        self.robot = 0
-        self.factory = 1
-        self.detector = 0
+	def __init__(self, broker, adapter, container):
+		self.broker = broker
+		self.adapter = adapter
+		self.container = container    
+		self.robot = 0
+		self.factory = 1
+		self.detector = 0
 
-    def makeDetectorController(self, current=None):
-        if self.detector == 0:
-            print "******** CREATING DETECTOR ********"
-        self.container.setType("ContainerDetector")
+	def makeDetectorController(self, current=None):
+		if self.detector == 0:
+			print "******** CREATING DETECTOR ********"
+		self.container.setType("ContainerDetector")
 
-        print ('Making a detector controller at the factory ' + str(self.factory))
+		print ('Making a detector controller at the factory ' + str(self.factory))
 
-        factory_proxy = self.broker.stringToProxy("detector")
-        factory = services.ControllerFactoryPrx.checkedCast(factory_proxy)
-        dc = factory.makeDetector(self.container)
-        print("{0}: link: {1} -> {2}".format(self.factory, factory_proxy, dc))
-        self.detector += 1
-        sys.stdout.flush()
-        return dc
+		factory_proxy = self.broker.stringToProxy("detector")
+		factory = services.ControllerFactoryPrx.checkedCast(factory_proxy)
+		dc = factory.makeDetector(self.container)
+		print("{0}: link: {1} -> {2}".format(self.factory, factory_proxy, dc))
+		self.detector += 1
+		sys.stdout.flush()
+		return dc
 
-    def makeController(self, robot, current=None): 
-        if self.robot == 0 :
-            print "******** CREATING CONTROLLERS ********"
-            self.container.setType("ContainerController")
+	def makeController(self, robot, current=None): 
+		if self.robot == 0 :
+			print "******** CREATING CONTROLLERS ********"
+			self.container.setType("ContainerController")
 
-        print ('Making a robot controller at the factory ' + str(self.factory))
+		print ('Making a robot controller at the factory ' + str(self.factory))
 
-        factory_proxy = self.broker.stringToProxy("factory"+str(self.factory))
-        factory = services.ControllerFactoryPrx.checkedCast(factory_proxy)
-        rc = factory.make(robot, self.container, self.robot)
-        print("{0}: link: {1} -> {2}".format(self.factory, factory_proxy, rc))
-        self.container.link(self.robot, rc)
-        self.robot += 1
-        self.factory += 1
+		factory_proxy = self.broker.stringToProxy("factory"+str(self.factory))
+		factory = services.ControllerFactoryPrx.checkedCast(factory_proxy)
+		rc = factory.make(robot, self.container, self.robot)
+		print("{0}: link: {1} -> {2}".format(self.factory, factory_proxy, rc))
+		self.container.link(self.robot, rc)
+		self.robot += 1
+		self.factory += 1
 
-        sys.stdout.flush()
-        return rc   
-    
-    def win(self, current=None): 
-        print "Player1 wins!"
-        current.adapter.getCommunicator().shutdown()
+		sys.stdout.flush()
+		return rc   
+	
+	def win(self, current=None): 
+		print "Player1 wins!"
+		current.adapter.getCommunicator().shutdown()
 
-    def lose(self, current=None):
-        print "Player1 loses!"
-        current.adapter.getCommunicator().shutdown()
+	def lose(self, current=None):
+		print "Player1 loses!"
+		current.adapter.getCommunicator().shutdown()
 
-    def gameAbort(self, current=None):
-        print "Game aborted. Exiting"
-        current.adapter.getCommunicator().shutdown()
+	def gameAbort(self, current=None):
+		print "Game aborted. Exiting"
+		current.adapter.getCommunicator().shutdown()
 
 if __name__ == '__main__':
 	sys.exit(PlayerApp().main(sys.argv))
--- DrobotsConIceGrid/code/Player2.py
+++ DrobotsConIceGrid/code/Player2.py
@@ -16,103 +16,103 @@ from RobotControllerDefender import *
 from DetectorController import *
 
 class PlayerApp(Ice.Application): 
-    def run(self, argv):
+	def run(self, argv):
 
-        broker = self.communicator()
+		broker = self.communicator()
 
-        #well-known object
-        container_proxy = broker.stringToProxy('container')
-        container = services.ContainerPrx.checkedCast(container_proxy)
+		#well-known object
+		container_proxy = broker.stringToProxy('container')
+		container = services.ContainerPrx.checkedCast(container_proxy)
 
-        adapter = broker.createObjectAdapter('PlayerAdapter')
-        adapter.activate()
+		adapter = broker.createObjectAdapter('PlayerAdapter')
+		adapter.activate()
 
-        player_servant = PlayerI(broker, adapter, container)
-        proxy_player = adapter.addWithUUID(player_servant)
-        print ('Proxy player: ' +str(proxy_player))
-        direct_player = adapter.createDirectProxy(proxy_player.ice_getIdentity())
-        player = drobots.PlayerPrx.uncheckedCast(direct_player)
+		player_servant = PlayerI(broker, adapter, container)
+		proxy_player = adapter.addWithUUID(player_servant)
+		print ('Proxy player: ' +str(proxy_player))
+		direct_player = adapter.createDirectProxy(proxy_player.ice_getIdentity())
+		player = drobots.PlayerPrx.uncheckedCast(direct_player)
 
-        proxy_game = broker.propertyToProxy('Player') 
-        print ('Proxy game: ' +str(proxy_game))
-        #game = drobots.GamePrx.checkedCast(proxy_game)
-        gameFact = drobots.GameFactoryPrx.checkedCast(proxy_game)
-        game = gameFact.makeGame("GameRobots", 2)
+		proxy_game = broker.propertyToProxy('Player') 
+		print ('Proxy game: ' +str(proxy_game))
+		#game = drobots.GamePrx.checkedCast(proxy_game)
+		gameFact = drobots.GameFactoryPrx.checkedCast(proxy_game)
+		game = gameFact.makeGame("GameRobots", 2)
 
-        try:
-            print ('Trying to do login...')
-            game.login(player, 'Pedro' + str(random.randint(0,99)))
-            print ('Waiting to receive both the robot controllers and the detector')
-        except drobots.GameInProgress:
-            print "\nGame in progress. Try it again"
-            return 1
-        except drobots.InvalidProxy:
-            print "\nInvalid proxy"
-            return 2
-        except drobots.InvalidName, e:
-            print "\nInvalid name. It is possible that other person be using your name"
-            print str(e.reason)
-            return 3
-        except drobots.BadNumberOfPlayers:
-            print "\nBad number of players"
-            return 4            
-        
-        sys.stdout.flush()
-        self.shutdownOnInterrupt()
-        broker.waitForShutdown()
-        return 0
+		try:
+			print ('Trying to do login...')
+			game.login(player, 'Pedro' + str(random.randint(0,99)))
+			print ('Waiting to receive both the robot controllers and the detector')
+		except drobots.GameInProgress:
+			print "\nGame in progress. Try it again"
+			return 1
+		except drobots.InvalidProxy:
+			print "\nInvalid proxy"
+			return 2
+		except drobots.InvalidName, e:
+			print "\nInvalid name. It is possible that other person be using your name"
+			print str(e.reason)
+			return 3
+		except drobots.BadNumberOfPlayers:
+			print "\nBad number of players"
+			return 4            
+		
+		sys.stdout.flush()
+		self.shutdownOnInterrupt()
+		broker.waitForShutdown()
+		return 0
 
 class PlayerI(drobots.Player):
-    def __init__(self, broker, adapter, container):
-        self.broker = broker
-        self.adapter = adapter
-        self.container = container    
-        self.rc_counter = 0
-        self.dc_counter = 0
+	def __init__(self, broker, adapter, container):
+		self.broker = broker
+		self.adapter = adapter
+		self.container = container    
+		self.rc_counter = 0
+		self.dc_counter = 0
 
-    def makeDetectorController(self, current=None):
-        print ('Making a controller detector...')
-        name = 'dc' + str(self.dc_counter)
-        dc_servant = DetectorControllerI(self.container)
+	def makeDetectorController(self, current=None):
+		print ('Making a controller detector...')
+		name = 'dc' + str(self.dc_counter)
+		dc_servant = DetectorControllerI(self.container)
  
-        dc_proxy = self.adapter.add(dc_servant, self.broker.stringToIdentity(name))
-        print dc_proxy
+		dc_proxy = self.adapter.add(dc_servant, self.broker.stringToIdentity(name))
+		print dc_proxy
 
-        dc_proxy = current.adapter.createDirectProxy(dc_proxy.ice_getIdentity())
-        dc = drobots.DetectorControllerPrx.checkedCast(dc_proxy)
-        self.dc_counter = self.dc_counter + 1
-        sys.stdout.flush()
-        return dc
+		dc_proxy = current.adapter.createDirectProxy(dc_proxy.ice_getIdentity())
+		dc = drobots.DetectorControllerPrx.checkedCast(dc_proxy)
+		self.dc_counter = self.dc_counter + 1
+		sys.stdout.flush()
+		return dc
 
-    def makeController(self, robot, current=None): 
-        print ('Making a robot controller...')
-        name = 'rc' + str(self.rc_counter)
-        self.rc_counter += 1
+	def makeController(self, robot, current=None): 
+		print ('Making a robot controller...')
+		name = 'rc' + str(self.rc_counter)
+		self.rc_counter += 1
 
-        if robot.ice_isA("::drobots::Attacker"):
-            rc_servant = RobotControllerAttackerI(robot, self.container, self.rc_counter)
-        else:
-            rc_servant = RobotControllerDefenderI(robot, self.container, self.rc_counter)
+		if robot.ice_isA("::drobots::Attacker"):
+			rc_servant = RobotControllerAttackerI(robot, self.container, self.rc_counter)
+		else:
+			rc_servant = RobotControllerDefenderI(robot, self.container, self.rc_counter)
  
-        rc_proxy = self.adapter.add(rc_servant, self.broker.stringToIdentity(name))
-        print rc_proxy
+		rc_proxy = self.adapter.add(rc_servant, self.broker.stringToIdentity(name))
+		print rc_proxy
 
-        rc_proxy = current.adapter.createDirectProxy(rc_proxy.ice_getIdentity())
-        rc = drobots.RobotControllerPrx.checkedCast(rc_proxy)
-        sys.stdout.flush()
-        return rc             
-    
-    def win(self, current=None): 
-        print "Player2 wins!"
-        current.adapter.getCommunicator().shutdown()
+		rc_proxy = current.adapter.createDirectProxy(rc_proxy.ice_getIdentity())
+		rc = drobots.RobotControllerPrx.checkedCast(rc_proxy)
+		sys.stdout.flush()
+		return rc             
+	
+	def win(self, current=None): 
+		print "Player2 wins!"
+		current.adapter.getCommunicator().shutdown()
 
-    def lose(self, current=None):
-        print "Player2 loses!"
-        current.adapter.getCommunicator().shutdown()
+	def lose(self, current=None):
+		print "Player2 loses!"
+		current.adapter.getCommunicator().shutdown()
 
-    def gameAbort(self, current=None):
-        print "Game aborted. Exiting"
-        current.adapter.getCommunicator().shutdown()
+	def gameAbort(self, current=None):
+		print "Game aborted. Exiting"
+		current.adapter.getCommunicator().shutdown()
 
 if __name__ == '__main__':
 	sys.exit(PlayerApp().main(sys.argv))
